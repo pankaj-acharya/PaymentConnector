0e1bf3e9ed73de2aff696d043d8f745503f2733a
Test 02 <test02@RBEIBSS.onmicrosoft.com>
2020-02-24 22:03:05 +0000

    -Added response mapper for CardPayment

--- Extension.EPSConnector/EPSConnector.cs
+++ Extension.EPSConnector/EPSConnector.cs
@@ -25,6 +25,7 @@ namespace Hardware.Extension.EPSPaymentConnector
     using Microsoft.Dynamics.Commerce.Runtime;
     using System.Net;
     using System.IO;
+    using System.Xml;
 
     /// <summary>
     /// <c>Simulator</c> manager payment device class.
@@ -484,7 +485,7 @@ namespace Hardware.Extension.EPSPaymentConnector
                 WriteLog("Entered method: AuthorizePaymentAsync", true);
 
                 //return _helper.PostXMLData(Helper.RequestType.Authorize, DateTime.UtcNow, "Clerk123", request.IsManualEntry, "TRNX234", request.Currency, request.Amount.ToString());
-                string urlwithPortAndPath = "http://localhost:8900/Capture";//_config.EndPointIp + destinationPath;
+                string urlwithPortAndPath = "http://127.0.0.1:8900/Capture";//_config.EndPointIp + destinationPath;
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlwithPortAndPath);
                 byte[] bytes;
                 string _xmlString = BuildSampleCardRequestXML();
@@ -505,6 +506,7 @@ namespace Hardware.Extension.EPSPaymentConnector
                     WriteLog($"HTTP Response status code :{response.StatusCode}");
                     Stream responseStream = response.GetResponseStream();
                     string responseStr = new StreamReader(responseStream).ReadToEnd();
+                    paymentInfo = MapCardPaymentResponse(responseStr);
                     WriteLog($"Response :{responseStr}");
                 }
             }
@@ -518,6 +520,73 @@ namespace Hardware.Extension.EPSPaymentConnector
             return paymentInfo;
         }
 
+        public PaymentInfo MapCardPaymentResponse(string responseStr)
+        {
+            PaymentInfo paymentInfo = new PaymentInfo {
+                IsApproved=false
+            };
+
+           // var xmlRequest = BuildSampleCardRequestXML();
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(responseStr);
+
+           
+            XmlNodeList cardServiceResponseNode = xmlDoc.GetElementsByTagName("CardServiceResponse");
+            string requestType = cardServiceResponseNode[0].Attributes["RequestType"].Value;
+            string workstationID = cardServiceResponseNode[0].Attributes["WorkstationID"].Value;
+            string requestID = cardServiceResponseNode[0].Attributes["RequestID"].Value;
+            string overallResult = cardServiceResponseNode[0].Attributes["OverallResult"].Value;
+
+            var terminalNode = xmlDoc.GetElementsByTagName("Terminal");
+            string terminalID = terminalNode[0].Attributes["TerminalID"].Value;
+            string terminalBatch = terminalNode[0].Attributes["TerminalBatch"].Value;
+
+            var tenderNode = xmlDoc.GetElementsByTagName("Tender");
+
+            var totalAmountNode = xmlDoc.GetElementsByTagName("TotalAmount");
+            string totalAmount = totalAmountNode[0].InnerText;
+
+            var authorisationNode = xmlDoc.GetElementsByTagName("Authorisation");
+            string acquirerID = authorisationNode[0].Attributes["AcquirerID"].Value;
+            string startDate = authorisationNode[0].Attributes["StartDate"].Value;
+            string expiryDate = authorisationNode[0].Attributes["ExpiryDate"].Value;
+            string timeStamp = authorisationNode[0].Attributes["TimeStamp"].Value;
+
+            #region Different Action codes as per EPS doc
+            // '004' is undefined by IFSF so should be treated as a decline.   
+            //'000' Approved 
+            //'001' Honour, with Identification Approved 
+            //'002' Approved for partial amount Approved 
+            //'003' Approved(VIP) Approved 
+            //'005' Approved, account type specified by card issuer 
+            //'006' Approved for partial amount, account Approved 
+            //'007' Approved, update ICC Approved
+            #endregion
+
+            string actionCode = authorisationNode[0].Attributes["ActionCode"].Value;
+            string approvalCode = authorisationNode[0].Attributes["ApprovalCode"].Value;
+            string acquirerBatch = authorisationNode[0].Attributes["AcquirerBatch"].Value;
+            string panprint = authorisationNode[0].Attributes["PANprint"].Value;
+            string merchant = authorisationNode[0].Attributes["Merchant"].Value;
+            string authorisationType = authorisationNode[0].Attributes["AuthorisationType"].Value;
+            string captureReference = authorisationNode[0].Attributes["CaptureReference"].Value;
+
+            if (overallResult.Equals(OverallResult.Success))
+                paymentInfo.IsApproved = true;
+               
+            return paymentInfo;
+        }
+        enum OverallResult
+        {
+            Success,
+            CommunicationError
+        }
+        enum EpsRequestType
+        {
+            CardPayment,
+            PaymentRefund,
+            AbortRequest
+        } 
         /// <summary>
         ///  Begins the transaction.
         /// </summary>
@@ -1211,7 +1280,10 @@ namespace Hardware.Extension.EPSPaymentConnector
 
         public string BuildSampleCardRequestXML()
         {
-            string sampleXmlAsString = "<?xml version=\"1.0\"?> <CardServiceRequest xmlns=\"http://www.nrf-arts.org/IXRetail/namespace\"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" RequestType=\"CardPayment\"  WorkstationID=\"Dave's Workstation\" RequestID=\"19\"> <POSdata> <POSTimeStamp>2020-02-05T15:02:59.6303603+01:00</POSTimeStamp> <ManualPAN>false</ManualPAN> </POSdata> < TotalAmount Currency = \"GBP\" > 9 </ TotalAmount > </ CardServiceRequest >";
+            // string sampleXmlAsString1 = "<?xml version=\"1.0\"?> <CardServiceRequest xmlns=\"http://www.nrf-arts.org/IXRetail/namespace\"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" RequestType=\"CardPayment\"  WorkstationID=\"Dave's Workstation\" RequestID=\"19\"> <POSdata> <POSTimeStamp>2020-02-05T15:02:59.6303603+01:00</POSTimeStamp> <ManualPAN>false</ManualPAN> </POSdata> < TotalAmount Currency = \"GBP\" > 9 </ TotalAmount > </ CardServiceRequest >";
+
+            //{DateTime.Now.ToString("yyyy-MM-ddTHH:mm:sszzz")}
+            string sampleXmlAsString = $"<?xml version =\"1.0\"?> <CardServiceRequest xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" RequestType=\"CardPayment\" WorkstationID=\"TestPos\" RequestID=\"2\" xmlns=\"http://www.nrf-arts.org/IXRetail/namespace\">  <POSdata> <POSTimeStamp>{DateTime.Now.ToString("yyyy-MM-ddTHH:mm:sszzz")}</POSTimeStamp> <ClerkID> 123456 </ClerkID> <ManualPAN> false </ManualPAN> <TransactionNumber> TXNDEMONo5 </TransactionNumber>   </POSdata>   <TotalAmount Currency=\"GBP\"> 7 </TotalAmount> </CardServiceRequest>";
 
             return sampleXmlAsString;
         }
--- Extension.EPSConnectorTests/EPSConnectorTests.cs
+++ Extension.EPSConnectorTests/EPSConnectorTests.cs
@@ -18,9 +18,22 @@ namespace Hardware.Extension.EPSPaymentConnector.Tests
             EPSConnector _connector = new EPSConnector();
 
             //Act
-
+            var xmlString=_connector.BuildSampleCardRequestXML();
             //Assert
-            Assert.Fail();
+            Assert.IsTrue(xmlString.Length > 0);
+        }
+
+        [TestMethod()]
+        public void ParseXMLDataTest()
+        {
+            //Arrange
+            EPSConnector _connector = new EPSConnector();
+            string xmlResponseString = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><CardServiceResponse xmlns=\"http://www.nrf-arts.org/IXRetail/namespace\" RequestType = \"CardPayment\" WorkstationID = \"Dave's Workstation\" RequestID = \"19\" OverallResult = \"Success\"><Terminal TerminalID = \"02052317\" TerminalBatch = \"006\"/><Tender><TotalAmount> 9.000000 </TotalAmount><Authorisation AcquirerID = \"000000\" StartDate = \"0401\" ExpiryDate = \"1412\" TimeStamp = \"2012-07-25T15:02:59.000+01:00\" ActionCode = \"000\" ApprovalCode = \"011860\" AcquirerBatch = \"006\" PANprint = \"541333******0036\" Merchant = \"2100112192\" AuthorisationType = \"Online\" CaptureReference = \"0092\"/></Tender></CardServiceResponse >";
+
+            //Act
+            var paymentInfo = _connector.MapCardPaymentResponse(xmlResponseString);
+            //Assert
+            Assert.IsNotNull(paymentInfo);
         }
     }
 }
